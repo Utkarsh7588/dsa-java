0f984d32b770d481856f778a437f5f7d79823a41
UVM <utkarshmore7588@gmail.com>
2025-08-28 12:59:15 +0530

    implementaion of queue using 2 stacks

---
+++ src/StackAndQue/ImplementQueUsingStack.java
@@ -0,0 +1,41 @@
+package StackAndQue;
+
+import java.util.Stack;
+
+public class ImplementQueUsingStack {
+    Stack<Integer> add;
+    Stack<Integer> remove;
+     public ImplementQueUsingStack() {
+        this.add=new Stack<>();
+        this.remove=new Stack<>();
+    }
+
+    public void push(int x) {
+        this.add.push(x);
+    }
+
+    public int pop() {
+        if(this.remove.isEmpty()){
+            while(!this.add.isEmpty()){
+                this.remove.push(this.add.pop());
+            }
+        }
+        return this.remove.pop();
+    }
+
+    public int peek() {
+        if(this.remove.isEmpty()){
+            while(!this.add.isEmpty()){
+                this.remove.push(this.add.pop());
+            }
+        }
+        return this.remove.peek();
+    }
+
+    public boolean empty() {
+        if(this.remove.isEmpty()&&this.add.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
