d04978394bdb1ac5edca206b863564e8a5abf396
UVM <utkarshmore7588@gmail.com>
2025-08-30 11:27:46 +0530

    largest rectangle in a histogram

---
+++ src/StackAndQue/LargestRectangle.java
@@ -0,0 +1,32 @@
+package StackAndQue;
+
+import java.util.Stack;
+
+public class LargestRectangle {
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int n = heights.length;
+
+        for (int i = 0; i <= n; i++) {
+            // Use 0 height for the "virtual bar" at the end
+            int currentHeight = (i == n) ? 0 : heights[i];
+
+            while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
+                int height = heights[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                int area = height * width;
+                maxArea = Math.max(maxArea, area);
+            }
+
+            stack.push(i);
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = {2, 1, 5, 6, 2, 3};
+        System.out.println("Largest Rectangle Area = " + largestRectangleArea(heights));
+    }
+}
